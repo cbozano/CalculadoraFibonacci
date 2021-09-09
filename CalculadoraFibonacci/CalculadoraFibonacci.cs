b8766b119a6d35496598e273e37d816a35ee6d3b
nettrim <nettrim@LAPTOP-JA8DDUEO>
2021-09-09 13:31:31 +0200

    Agregar archivos de proyecto.

---
+++ CalculadoraFibonacci/CalculadoraFibonacci.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraFibonacci
+{
+    class CalculadoraFibonacci
+    {
+        public double Suma(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Fibonacci(int numero)
+        {
+            if (numero <= 1)
+            {
+                return numero;
+            }
+            else
+            {
+                return Fibonacci(numero - 1) + Fibonacci(numero - 2);
+            }
+
+
+        }
+        public static int Factorial(int numero)
+        {
+            if (numero <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return numero * Factorial(numero - 1);
+            }
+        }
+    }
+}
---
+++ CalculadoraFibonacci/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CalculadoraFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 6;
+
+            Calculadora calculo = new Calculadora();
+
+            double result = calculo.Suma(num1, num2);
+
+
+            Console.WriteLine(result);
+        }
+    }
+}
